c3e95f2fd47cee347c92fe4ee8837886e0655395
Ryan Shin <1982sdr@hanmail.net>
2016-02-25 17:48:21 +0900

    BidWeb - custom templatetags 적용.

--- BidWeb/settings.py
+++ BidWeb/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bid'
+    'bid',
+    'bid.templatetags.snippets'
 ]
 
 MIDDLEWARE_CLASSES = [

e7f299a85a04eb48c4acade15652da773c4de1db
Ryan Shin <1982sdr@hanmail.net>
2019-01-02 07:30:41 +0900

    BidWeb - Django 2.0 이상을 위한 on_delete=models.CASCADE 추가.

--- bid/models.py
+++ bid/models.py
@@ -7,14 +7,14 @@ import json
 class Member(models.Model):
     id = models.CharField(max_length=20, primary_key=True)
     created_date = models.DateTimeField()
-    
+
 class BidItem(models.Model):
     name = models.CharField(max_length=50)
     data = models.CharField(max_length=1024)
-    member = models.ForeignKey(Member)
-    
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+
     def setdata(self, x):
         self.data = json.dumps(x)
-    
+
     def getdata(self):
         return json.loads(self.data)
